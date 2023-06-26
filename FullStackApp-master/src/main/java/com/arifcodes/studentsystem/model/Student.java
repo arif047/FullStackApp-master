6277146ed30e7485b4b27eaf1d9d3800ed1604a4
arif047 <94097287+arif047@users.noreply.github.com>
2023-06-26 15:27:26 +0600

    Create Student.java

---
+++ FullStackApp-master/src/main/java/com/arifcodes/studentsystem/model/Student.java
@@ -0,0 +1,86 @@
+package com.arifcodes.studentsystem.model;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
+public class Student {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+    private String name;
+    private String email;
+
+    private String password;
+
+    private String departmentname;
+
+    private String phone;
+
+    private String advisor_id;
+
+    public Student() {
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getPhone() {
+        return phone;
+    }
+
+    public void setPhone(String phone) {
+        this.phone = phone;
+    }
+
+    public String getDepartmentname() {
+        return departmentname;
+    }
+
+    public void setDepartmentname(String departmentname) {
+        this.departmentname = departmentname;
+    }
+
+
+    public String getAdvisor_id() {
+        return advisor_id;
+    }
+
+    public void setAdvisor_id(String advisor_id) {
+        this.advisor_id = advisor_id;
+    }
+
+
+}
